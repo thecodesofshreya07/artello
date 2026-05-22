ce59eb89b84cd6c9add5a36d84f31c67014c0c9c
Shreya Mishra <sm8054800@gmail.com>
2026-05-23 00:30:36 +0530

    Initial commit

---
+++ src/socket.js
@@ -0,0 +1,6 @@
+//connecting frontend to server
+import { io } from "socket.io-client";
+export const socket = io("http://localhost:5000");
+socket.on("connect", () => {
+   console.log("Connected to server:", socket.id);
+});
