7ef3518d0a23115253cc36f95eef9a4d28a64773
Shreya Mishra <sm8054800@gmail.com>
2026-07-11 21:52:19 +0530

    undo fix

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,8 +1,21 @@
 import { io } from "socket.io-client";
 
-export const socket = io(import.meta.env.VITE_API_URL || "http://localhost:5000");
+const getSocketUrl = () => {
+  if (import.meta.env.VITE_API_URL) {
+    return import.meta.env.VITE_API_URL;
+  }
+  const origin = window.location.origin;
+  // If the browser was loaded from a local IP address (like http://192.168.1.100:5173),
+  // connect the socket to that same IP on port 5000!
+  const ipMatch = origin.match(/http:\/\/(\d+\.\d+\.\d+\.\d+)/);
+  if (ipMatch) {
+    return `http://${ipMatch[1]}:5000`;
+  }
+  return "http://localhost:5000";
+};
+
+export const socket = io(getSocketUrl());
 
 socket.on("connect", () => {
   console.log("Connected to server:", socket.id);
-  
 });
